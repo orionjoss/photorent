53c32c51e68323abcbc79f9d37eb47dda5ec92b5
Hugo Pereira <hugop1997@gmail.com>
2023-08-22 13:01:43 +0100

    Created Bookings Controller and respective Actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/update'
+  get 'bookings/edit'
+  get 'bookings/delete'
+  get 'bookings/show'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
