f68a6b14a500d158c3c7e1b3b8919fd2210fe2b5
orionjoss <orionjoss@gmail.com>
2023-08-22 15:05:26 +0100

    Creating seed database for users, cameras and bookings using Faker gem

--- db/schema.rb
+++ db/schema.rb
@@ -38,5 +38,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_105944) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table “bookings_tables”, force: :cascade do |t|
+    t.date “start_date”
+    t.date “end_date”
+    t.string “status”
+    t.bigint “user_id”, null: false
+    t.bigint “camera_id”, null: false
+    t.datetime “created_at”, null: false
+    t.datetime “updated_at”, null: false
+    t.index [“camera_id”], name: “index_bookings_tables_on_camera_id”
+    t.index [“user_id”], name: “index_bookings_tables_on_user_id”
+  end
+
   add_foreign_key "cameras", "users"
 end
