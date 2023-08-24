d8ef71d7a695deaf008c41ca4a534f7cc42eef17
orionjoss <orionjoss@gmail.com>
2023-08-24 10:07:08 +0100

    enhanced search with pg search + home button to navbar

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -6,4 +6,14 @@ class Camera < ApplicationRecord
   validates :brand, presence: true
   validates :camera_type, presence: true
   validates :address, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+                  against: [ :brand, :camera_type ],
+                  associated_against: {
+                    user: [ :first_name, :last_name ]
+                  },
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
