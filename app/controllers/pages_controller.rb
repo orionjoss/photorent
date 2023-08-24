645f2429d0ec3d01599a9513ad54f45168789577
jorge marques <jorgetmarques@gmail.com>
2023-08-24 11:39:25 +0100

    sketched listing page, fixed cloudinary, added upload picture feature

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
+    redirect_to cameras_path if user_signed_in?
   end
 end
--- app/models/camera.rb
+++ app/models/camera.rb
@@ -1,6 +1,8 @@
 class Camera < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_one_attached :photo
+  # has_many_attached :photos
 
   validates :brand, presence: true
   validates :camera_type, presence: true
