4566f0a7788669fbdd0cf55bf1a9f96e72afce3e
orionjoss <orionjoss@gmail.com>
2023-08-24 16:15:01 +0100

    change default value of bookings status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_180244) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_150300) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_180244) do
   create_table "bookings", force: :cascade do |t|
     t.date "start_date"
     t.date "end_date"
-    t.string "status"
+    t.string "status", default: "Pending"
     t.bigint "user_id", null: false
     t.bigint "camera_id", null: false
     t.datetime "created_at", null: false
